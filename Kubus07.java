05d66bcc087fc509dced858d8fffd5a6f2ba4b53
Aureylio <aureyliorangga2@gmail.com>
2025-11-30 15:29:56 +0700

    Tugas No 1

--- Kubus07.java
+++ Kubus07.java
@@ -1,17 +1,24 @@
 public class Kubus07 {
     
+    // Fungsi untuk menghitung volume kubus
+    // Rumus volume kubus = sisi * sisi * sisi (s^3)
     static int volume(int sisi){
         return sisi * sisi * sisi;
     }
 
+    // Fungsi untuk menghitung  luas permukaan kubus
+    // Rumus luas permukaan kubus = 6 * sisi * sisi (6s^2)
     static int luasPermukaan(int sisi){
-        return 6 * sisi * sisi;
+        return 6 * sisi * sisi; // Mengembalikan luas permukaan kubus
     }
 
     public static void main(String[] args) {
-        int s = 5;
+        int s = 5; // Nilai sisi kubus (contoh: sisi = 5)
 
+        // Memanggil fungsi volume() dan menampilkan hasilnya
         System.out.println("Volume Kubus = " + volume(s));
+        
+        // Memanggil fungsi luasPermukaan() dan menampilkan hasilnya
         System.out.println("Luas Permukaan = " + luasPermukaan(s));
     }
 }
